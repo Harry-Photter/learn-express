b620fa01aefb79ba51df4b0bfd01278c1b5adb67
Mateusz Furman <mateuszfurman.nf@gmail.com>
2021-03-31 15:57:49 +0200

    Correct middleware

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/user', (req, res, next) => {
-  if(isAdmin()) next();
-  else res.send('Go away!');
+  res.show('forbidden.html');
 });
 
 app.use(express.static(path.join(__dirname, `/public`)));
@@ -23,14 +22,6 @@ app.get(`/`, (req, res) => {
 
 app.get(`/about`, (req, res) => {
   res.show('about.html');
-});
-
-app.get('/user/settings', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/forbidden.html'));
-});
-
-app.get('/user/panel', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/forbidden.html'));
 });
 
 app.use((req, res) => {
