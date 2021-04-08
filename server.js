ac48fb0ae3c0f77f432806ec5816969cb026df07
Mateusz Furman <mateuszfurman.nf@gmail.com>
2021-04-08 21:28:15 +0200

    Add templates and layouts

--- server.js
+++ server.js
@@ -1,33 +1,43 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.use('/user', (req, res, next) => {
-  res.show('forbidden.html');
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
-app.use(express.static(path.join(__dirname, `/public`)));
-
-app.get(`/`, (req, res) => {
-  res.show('home.html');
+app.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
-app.get(`/about`, (req, res) => {
-  res.show('about.html');
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history', { layout: 'dark' });
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, '/views/404.html'));
+  res.status(404).send('404 not found...');
 })
 
-app.listen(8080, () => {
+app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
+
+
